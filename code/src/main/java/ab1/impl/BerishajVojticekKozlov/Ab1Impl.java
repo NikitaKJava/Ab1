177bad61a7ea58a18b6f842b4c8b4a1de205672a
florianvo6 <florian.vojticek@gmx.at>
2023-04-22 18:17:16 +0200

    Create Ab1Impl.java

---
+++ code/src/main/java/ab1/impl/BerishajVojticekKozlov/Ab1Impl.java
@@ -0,0 +1,34 @@
+package ab1.impl.BerishajVojticekKozlov;
+
+import ab1.Ab1;
+import ab1.Heap;
+import ab1.SortedList;
+import ab1.Sorter;
+
+public class Ab1Impl implements Ab1 {
+
+    @Override
+    public SortedList newSortedList() {
+        // Ihre Implementierung
+        return null;
+    }
+
+    @Override
+    public Sorter newQuickSort() {
+        // Ihre Implementierung
+        return null;
+    }
+
+    @Override
+    public Heap newHeap(int capacity) {
+        // Ihre Implementierung
+        return null;
+    }
+
+    @Override
+    public Sorter newHeapSort(int capacity) {
+        // Ihre Implementierung
+        return null;
+    }
+
+}
